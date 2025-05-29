029c391bc4a64564263978f0cf8b6ba55942fa0c
yoyojuls-dev <visbaljulius@gmail.com>
2025-05-29 21:15:21 +0800

    Add files via upload

---
+++ admin/footer.php
@@ -0,0 +1,16 @@
+<div class="setfooter">
+		<footer >
+           <p align="center"><h3 align="center"> &copy; Church manager <?php
+ $date = new DateTime();
+ echo $date->format(' Y');
+ ?> </h3></p>
+		 <h3> <p align="center"><h3 align="center"><strong> Designed by: <a href="https://www.facebook.com/juluis.visbal" target="_blank" >DeeSheAly Do</a></h3></strong></p>
+		  		  <h3 align="center"> Follow Us :</h3> <p align="center">
+<a href="https://www.facebook.com/juluis.visbal" ><img src="images/facebook.png" id=social /></a>
+<a href="https://x.com/DiasNegati65406"><img src="images/twitter.png" id=social /></a>
+<a href="https://www.linkedin.com/in/julius-ceasar-visbal-547903295/"><img src="images/linkedin.png"id=social  /></a>
+<a href="https://www.youtube.com/@DeeSheAlyDo"><img src="images/youtube.png" id=social /></a>
+<a href="#https://myaccount.google.com/u/1/?hl=en&utm_source=OGB&utm_medium=act&gar=WzEyMF0"><img src="images/gmail.png" id=social /></a></p>
+</div>
+        <footer>
+</div>
